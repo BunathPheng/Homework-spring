afb22d267366180e3ed3cf868af06efa24104c37
Bunath Pheng <bunathpheng4@gmail.com>
2025-08-13 15:34:53 +0700

    last

--- src/main/java/com/example/jpa_homework/CorsConfig.java
+++ src/main/java/com/example/jpa_homework/CorsConfig.java
@@ -1,4 +1,21 @@
 package com.example.jpa_homework;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // allow all endpoints
+                        .allowedOrigins("*") // allow all domains
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
--- src/main/java/com/example/jpa_homework/SwaggerConfig.java
+++ src/main/java/com/example/jpa_homework/SwaggerConfig.java
@@ -1,4 +1,17 @@
 package com.example.jpa_homework;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.buanth.site"); // your domain
+        server.setDescription("Production Server");
+        return new OpenAPI().servers(List.of(server));
+    }
 }
